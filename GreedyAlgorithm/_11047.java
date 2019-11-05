d3ec7186965d0d12b865501d18234e99c21638c3
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-11-05 17:19:02 +0900

    solve

--- GreedyAlgorithm/_11047.java
+++ GreedyAlgorithm/_11047.java
@@ -5,52 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Algorithm{
-    int man;
-    StringTokenizer st;
-    int order[] = null;
-    Algorithm(int man,StringTokenizer st){
-        this.man = man;
-        this.st = st;
-        order = new int[man];
-        for (int i = 0; i < man; i++) {
-             order[i] = Integer.parseInt(st.nextToken());          
-    
-        }
-    }
-    
-    void order(){
-        Arrays.sort(order);
-        int sum = 0;
-        int result = 0;
-        for (int i = 0; i < order.length; i++) {
-            //System.out.print(order[i]+ " ");
-            sum += order[i];   
-            result += sum;
-        }
-        System.out.print(result);
-        
-    }
-}
-
 public class _11047 {
 
     public static void main(String[] args){
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        System.out.print("input the number of man : ");
-        int man = sc.nextInt();
-        try{
-            System.out.print("input the time in order : ");
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st = new StringTokenizer(br.readLine());
-            Algorithm ag = new Algorithm(man, st);
-            ag.order();
-        } catch (IOException e){
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int coin[] = new int[N];
+            for (int i = 0; i < N; i++) 
+                coin[i] = Integer.parseInt(br.readLine());
+            int count=0;
+            while(K != 0){
+                    for (int i = coin.length - 1; i >= 0; i--) {
+                        if(coin[i] <= K){
+                            K -= coin[i];
+                            break;
+                        }
+                    }
+                    count++;
+
+            }
+            System.out.println(count);
+            
+            
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        sc.close();
         
 
     
--- GreedyAlgorithm/_11399.java
+++ GreedyAlgorithm/_11399.java
@@ -5,52 +5,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class min_time{
-    int man;
-    StringTokenizer st;
-    int order[] = null;
-    min_time(int man,StringTokenizer st){
-        this.man = man;
-        this.st = st;
-        order = new int[man];
-        for (int i = 0; i < man; i++) {
-             order[i] = Integer.parseInt(st.nextToken());          
-    
-        }
-    }
-    
-    void order(){
-        Arrays.sort(order);
-        int sum = 0;
-        int result = 0;
-        for (int i = 0; i < order.length; i++) {
-            //System.out.print(order[i]+ " ");
-            sum += order[i];   
-            result += sum;
-        }
-        System.out.print(result);
-        
-    }
-}
 
-public class Greedy_11399 {
+public class _11399 {
 
     public static void main(String[] args){
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        System.out.print("input the number of man : ");
-        int man = sc.nextInt();
-        try{
-            System.out.print("input the time in order : ");
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int man = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
-            min_time mt = new min_time(man, st);
-            mt.order();
-        } catch (IOException e){
+            int order[] = new int[man];
+            for (int i = 0; i < man; i++)
+                order[i] = Integer.parseInt(st.nextToken());
+            Arrays.sort(order);
+            int sum = 0;
+            int result = 0;
+            for (int i = 0; i < order.length; i++) {
+                sum += order[i];
+                result += sum;
+            }
+            System.out.println(result);
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        sc.close();
         
 
     
