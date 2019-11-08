80f6e691d2173f2456fe611f8ae89716dcb6d408
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-11-08 17:05:31 +0900

    191108

--- GreedyAlgorithm/_1120.java
+++ GreedyAlgorithm/_1120.java
@@ -19,6 +19,18 @@ import java.util.StringTokenizer;
 // gjk 를 왼쪽에 넣거나 오른쪽에 넣을을때 가운데가 맞는다는 보장은 무엇으로 할수있는가?
 // 결론적으로 두개만 비교해보면된다.
 // 개수의 차이만큼 왼쪽에서 투입시키면된다. 
+//  aa dgdsdgjkl
+//     aa     aa
+// 다른방법이 있을까? 더 최소화시킬수있는방법 
+//   dd  ggddgg
+//   앞뒤로 ggddgg 시켜주면 가능하다.
+// 그러므로 내가 했던 방식은 다른방식이다.
+// 가장 최소화가되는 방향에서 앞뒤로 추가해줘야함
+// 일단은 자신이랑 비슷한것을 찾고 그뒤로 앞뒤로 추가
+// 1. 자신이랑 비슷한것을 찾는다.
+// 2. 앞뒤로 추가해준다.
+// 
+
 
 public class _1120 {
     public static void main(String[] args) throws IOException, NumberFormatException {
@@ -26,62 +38,52 @@ public class _1120 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         String A = st.nextToken();
         String B = st.nextToken();
-        int compare=0;
-        int compare2=0;
         List<String> m = new ArrayList<String>();
         List<String> m2 = new ArrayList<String>();
 
         for (int i = 0; i < A.length(); i++) {
-            m.add(String.valueOf(A.charAt(i)));    
+            m.add(String.valueOf(A.charAt(i)));    // String A []  ==> list m
         }
 
         for (int i = 0; i < B.length(); i++) {
-            m2.add(String.valueOf(B.charAt(i)));  
+            m2.add(String.valueOf(B.charAt(i)));   // String B []  ==> list m2
         }
 
-        int sizesub = m2.size() - m.size();
+        // A랑 B랑 비교해서 가장 비슷한 부분을 찾기
+        // bb  ggbbgg 있을때
+        // A의 항 A.legnth (자기자신)만큼 비교하면된다.
+        // B의 사이즈 끝까지    
 
-        for (int i = 0; i < sizesub; i++) { // right side add
-            m.add(m2.get( m2.size() - sizesub + i ));
-        }
+        int min=999;
+        int count=0;
+        int k=0;
+        for (int j = 0; j <= m2.size() - m.size(); j++) {       // 0.1.2.3.4.5.6 언제까지? 끝이 될??까지 끝은 언제되나? 전체사이즈에서 - m.size()까지
+            // System.out.println("m2.size - m.size : " + (m2.size() - m.size()));
+            for (int i = j ; i < j + m.size(); i++,k++) {    // j부터 J + m.size()까지하고 k 는 따로 설정하여서 m을 항상 0부터 자기사이즈까지 비교하기
+                // System.out.println("k : " + k + " i : " + i + " j : " + j);
+                if (m.get(k).equals(m2.get(i))) { // 0,j 1,j+1, 2,j+2 3,j+3 4,j+4  /*0,1,2,3,4... m.size() 까지*/ 
 
-        System.out.println(m);
-        System.out.println(m2);
-        for (int i = 0; i < m.size(); i++) {
-            if( m.get(i).equals(m2.get(i)) ){
-
+                } else {
+                    count++; // 각각을 비교해서 가장 낮은것의 수치를 적어주면된다. 그리고 해당수치에서 앞뒤로 추가해주면 그것이 가장 최적임
+                } // 가장 낮은수치를 계속 비교하여서 (temp) min값을 구하면됨
             }
-            else{
-                compare++;
-            } 
-        }
-
-        for (int i = 0; i < sizesub; i++) { // right side remove
-            m.remove( m2.size() - sizesub );
-        }
-        
-        for (int i = 0; i < sizesub; i++) { // left side add
-            m.add(i,m2.get(i));
-        }
-
-        System.out.println(m);
-        System.out.println(m2);
-        for (int i = 0; i < m.size(); i++) {
-            if( m.get(i).equals(m2.get(i)) ){
-
+            if(min >= count){ // 한번 비교가 끝나면 그값을 전값과 비교                
+                min = count;
             }
-            else{
-                compare2++;
-            } 
+            // System.out.println(min);
+            count=0;  // count초기화
+            k=0;
         }
 
-        if(compare >= compare2) System.out.println(compare2);
-        else{
-           System.out.println(compare);
-        }
-        
-        
+        System.out.println(min);
 
+        // 해당 비교가 끝나면 앞뒤로 추가해주었을때의 값은
+        // bb ggbbgg 일때 앞뒤로 무엇이 추가되는지는 내 알바가 아니지만 최소 A와 B의 차이만큼은 보장해줄수있음
+        // bb bb  어쨌든 4보장
+        //     bb  어쨋든 4보장
+        //      bb  어?든 4보장
+        
+        // System.out.println( (B.length() - A.length()) - 1 + min );
 
     }
 }
---
+++ GreedyAlgorithm/_1541.java
@@ -0,0 +1,98 @@
+package GreedyAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+세준이는 양수와 +, -, 그리고 괄호를 가지고 길이가 최대 50인 식을 만들었다. 그리고 나서 세준이는 괄호를 모두 지웠다.
+그리고 나서 세준이는 괄호를 적절히 쳐서 이 식의 값을 최소로 만들려고 한다.
+괄호를 적절히 쳐서 이 식의 값을 최소로 만드는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 식이 주어진다. 식은 ‘0’~‘9’, ‘+’, 그리고 ‘-’만으로 이루어져 있고, 가장 처음과 마지막 문자는 숫자이다. 
+그리고 연속해서 두 개 이상의 연산자가 나타나지 않고, 5자리보다 많이 연속되는 숫자는 없다. 수는 0으로 시작할 수 있다.
+
+입력          출력
+55-50+40     -35
+*/
+
+/*
+예를들어서 124+31-353+205 괄호가 + - 에 영향을 줄수있나?
+만약에 (12+20) 이런식일때 만약에 곱셈으로 작용이 가능하다면?
+아니다. 곱셈을 뜻하는것이 아니다.
+55 - 50 + 40 에서 - 50 다음에 괄호는 치면 된다.
+어디까지 ? - 가 나올때까지
+
+325 + 235 - 3535 - (2352 + 12 +  3535) - 3535 + 
+
+1) -를 찾는다. 그다음 마이너가 나올때까지 연산자를 전부 -로 바꾼다.
+2) 값을 구한다.
+
+*/
+
+public class _1541 {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String cal = br.readLine();
+        List<String> Num = new ArrayList<String>();
+        for (int i = 0; i < cal.length(); i++) {
+            Num.add(cal.charAt(i) + ""); // 전부빼서 리스트에 저장하기
+        }
+
+        int k = 0;
+        for (int i = k; i < cal.length(); i++) {
+            if (Num.get(i).equals("-")) {
+                for (int j = i; j < cal.length(); j++) { // - 를 찾았으면 그 다음에 연산자가 있는지 찾는 과정 필요
+                                                         // 연사자는 +,- 로 이루어져있음
+                    if (Num.get(j).equals("+")) {
+                        Num.set(j, "-"); // list 수정이 필요함
+                    }
+
+                    if (Num.get(j).equals("-")) { // 만약에 - 라면 다시 원래대로 for문으로 돌아감 그 자리를 찾아줌
+                        k = j; // 해당 찾은것부터 다시 검색
+                    }
+                }
+            }
+        }
+        
+        int total = 0;
+        boolean plus = true;
+        int piv = 0;
+        cal = "0";
+        // 첫항은 ture다. piv는 0부터 찾은항i까지
+        // 두번재항은 전에찾은 연산자부터
+        // System.out.println(Num);
+        for (int i = 0; i < Num.size(); i++) {
+            if (Num.get(i).equals("+") || Num.get(i).equals("-")){ //연산자라면
+                // System.out.println(Num.get(i)); 
+                if (plus == true)
+                    total += Integer.parseInt(cal); 
+                if (plus == false)
+                    total -= Integer.parseInt(cal); 
+
+                if(Num.get(i).equals("+")) plus=true;
+                if(Num.get(i).equals("-")) plus=false;
+                cal=""; // cal 초기화
+            }
+            else{
+                cal += Num.get(i); // string 숫자 만듬
+            }
+            // System.out.println("Total : " + total + " i =" + i);
+
+        }
+        if (plus == true)
+        total += Integer.parseInt(cal); 
+         if (plus == false)
+        total -= Integer.parseInt(cal); 
+            
+
+        System.out.println(total);
+
+    }
+
+}
