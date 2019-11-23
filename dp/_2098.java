2cb93d55f1b86351ea7334145a04aa08e5e1911d
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-11-23 16:19:49 +0900

    191123

--- dp/_2098.java
+++ dp/_2098.java
@@ -1,10 +1,55 @@
 package dp;
 
-public class _2098{
+import java.io.IOException;
+import java.util.Scanner;
 
-    public static void main(String args[]){
+public class _2098 {
+    static int map[][] =new int[16][16];
+    static int dp[][] = new int[16][1<<16]; // [16][65536]
+    static int n,visited;
+    static int INF = Integer.MAX_VALUE - 1000000;
 
-        System.out.println(Integer.toBinaryString(1 << 16));
+    static int tsp(int current, int visited){
+
+        if((visited == (1 << n) - 1 )){
+            if(map[current][0] == 0){
+                return INF;
+            }
+            return map[current][0];
+        }
+
+        if(dp[current][visited] != 0 ){
+            return dp[current][visited];
+        }
+        
+        dp[current][visited] = INF;
+
+        for(int k = 0 ; k < n ; k++){
+            int next = 1 << k;
+            if(map[current][k] == 0 || (visited & next) > 0){
+                continue;
+            }
+
+            dp[current][visited] = Math.min(dp[current][visited], tsp(k, visited | next) + map[current][k]);
+
+        }
+
+        return dp[current][visited];
+
+    }
+
+    public static void main(String args[]) throws NumberFormatException, IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(sc.next());
+            }
+        }
+
+        System.out.println(tsp(0,1));
+        
     }
 
 }
--- dp/_2133.java
+++ dp/_2133.java
@@ -3,7 +3,7 @@ package dp;
 public class _2133{
     
     public static void main(String args[]){
-
+        
 
     }
 }
