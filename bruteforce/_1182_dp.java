4bf588b384dde019a07701b663f42314c87d4145
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-12-08 00:20:01 +0900

    191207_night

--- bruteforce/_1182_dp.java
+++ bruteforce/_1182_dp.java
@@ -1,8 +1,6 @@
 package bruteforce;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class _1182_dp {
--- dp/_11053.java
+++ dp/_11053.java
@@ -1,68 +1,54 @@
 package dp;
 
-/*
-
-어떠한 알고리즘이 해당문제를 푸는데 최적화된 알고리즘인것을 보장할수있는것은 어떻게 알수가 있을까?
-
-문제를 풀다보니 회의감이 든다. 마치 LIS라는 문제라서 LIS로 풀고 DP문제라 DP로 풀고 어떠한 알고리즘이랑 주제에 맞춰서 해당 주제이기때문에 해당 LIS DP 등 해당 알고리즘이 타당한것처럼 보인다.
-하지만 이런 알고리즘이 타당한지 타당하지 않는지에 대한 확인은 누가해줄수있는것인가?
-
-문제를 푼사람들은 많지만 전부다 직감에 의해 또는 이러이러할것같아서 하는식으로 시작하여서
-마치 그것이 정답인것마냥 코딩을 한 사람들이 많다.
-
-
-하지만 이것이 정답인지 아닌지랑 상관없이 과정이 타당한것임을 확인해주는것은 누가해줄수있는가?
-
-*/
-
-
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
+public class _11053 {
 
-public class _11053{
+    static int d[][];
+    static int n[] = new int[1001];
 
-    static int n[];
-    static int dap=0;
-    static int count;
-    static int min=0;
-    static boolean max[] = new boolean[1001];
-    static void dfs(int hgap,int flag,int depth,int dap){
-        if(depth == count){
-            System.out.println(dap);
-            min = Math.max(min, dap);
-            max[dap] = true;
-            return;
+    public static void main(String args[]) throws IOException, NumberFormatException {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            n[i] = sc.nextInt();
         }
-        // if(hgap < n[depth]){
-        if(flag == 1 && hgap >= n[depth]){
-            return;
-        }
-        else{
-            dfs(hgap, 0, depth + 1,dap);
-            dfs(n[depth], 1, depth + 1,dap+1);
-        }
-
-            
+        // 0 10 20 30 10
+        d = new int[ 1 << num ][2];
         
-    }
+        int count = 1;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 0; j < 1 << (i-1); j++) {
+                d[count][0] = n[i];
+                count++;
+            }
+        }
+        for (int i = 0; i < 1 << num; i++) {
+            System.out.println(d[i][0]);
+        }
+        count=1;
+        d[0][0] = 0;
+        d[0][1] = 0;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 0; j < 1 << (i-1); j++) {
+                if (n[i] > d[j][0])
+                    d[count][1] = d[j][1] + 1;
+                else
+                    d[count][1] = d[j][1];
+                // System.out.print(d[count][1]);
+                count++;
+            }
+            // System.out.println();
+        }
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String q[] = br.readLine().split(" ");
-        count = Integer.parseInt(q[0]);
-        n = new int[1001];
-        q = br.readLine().split(" ");
-        for(int i = 0 ; i < count ; i++)
-            n[i] = Integer.parseInt(q[i]);
+        System.out.println(d[ (1 << num) - 1][1]);
 
-        dfs(0,0,0,0);
-        System.out.println(min);
-       
+        /*
+        6
+        10 20 10 30 20 50
 
-
-
+        */
     }
 }
