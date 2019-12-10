5416221e941d83380d99adb94f5ccd83d7eb015b
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-12-10 18:13:44 +0900

    191210

--- dp/_11053.java
+++ dp/_11053.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class _11053 {
 
-    static int d[][];
+    static int d[] = new int[1001];
     static int n[] = new int[1001];
 
     public static void main(String args[]) throws IOException, NumberFormatException {
@@ -15,40 +15,23 @@ public class _11053 {
         for (int i = 1; i <= num; i++) {
             n[i] = sc.nextInt();
         }
-        // 0 10 20 30 10
-        d = new int[ 1 << num ][2];
+        int temp;
+        int dap=0;
+
+        for (int i = 1; i < n.length; i++) {
+            temp = 1;
+            for (int j = 1; j < i; j++) {
+                if(n[i] > n[j])
+                    temp = Math.max(d[j]+1, temp);
+            }
+            d[i] = temp;
+            dap = Math.max(d[i], dap);
+
+        }
+
         
-        int count = 1;
-        for (int i = 1; i <= num; i++) {
-            for (int j = 0; j < 1 << (i-1); j++) {
-                d[count][0] = n[i];
-                count++;
-            }
-        }
-        for (int i = 0; i < 1 << num; i++) {
-            System.out.println(d[i][0]);
-        }
-        count=1;
-        d[0][0] = 0;
-        d[0][1] = 0;
-        for (int i = 1; i <= num; i++) {
-            for (int j = 0; j < 1 << (i-1); j++) {
-                if (n[i] > d[j][0])
-                    d[count][1] = d[j][1] + 1;
-                else
-                    d[count][1] = d[j][1];
-                // System.out.print(d[count][1]);
-                count++;
-            }
-            // System.out.println();
-        }
+        System.out.println(dap);
 
-        System.out.println(d[ (1 << num) - 1][1]);
-
-        /*
-        6
-        10 20 10 30 20 50
-
-        */
+        sc.close();
     }
 }
