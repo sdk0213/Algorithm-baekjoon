14f15a48f5ea2871e0629c77b6d30ba418c0ffcb
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-12-11 18:48:35 +0900

    191211

--- dp/_11053.java
+++ dp/_11053.java
@@ -2,36 +2,47 @@ package dp;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class _11053 {
 
-    static int d[] = new int[1001];
-    static int n[] = new int[1001];
+    static int d[] = new int[1001]; // lis값을 위한것
+    static int n[] = new int[1001]; // 값 받는것
+    static int A[] = new int[1001]; // 최솟값을위한것
 
     public static void main(String args[]) throws IOException, NumberFormatException {
 
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        for (int i = 1; i <= num; i++) {
-            n[i] = sc.nextInt();
-        }
-        int temp;
-        int dap=0;
 
-        for (int i = 1; i < n.length; i++) {
-            temp = 1;
-            for (int j = 1; j < i; j++) {
-                if(n[i] > n[j])
-                    temp = Math.max(d[j]+1, temp);
-            }
-            d[i] = temp;
-            dap = Math.max(d[i], dap);
+        Vector<Integer> vt = new Vector<Integer>();
+        
+        
+        vt.addElement(1);
+        
+        System.out.println(vt.size());
 
-        }
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+        // for (int i = 1; i <= num; i++) {
+        //     n[i] = sc.nextInt();
+        // }
+
+        // d[0] = 0; // 초기값
+        // A[0] = 0;
+        // int hgap = 0;
+
+        // int temp;
+        // int dap=0;
+
+        // for (int i = 1; i < n.length; i++) {
+        //     for (int j = 0; j <= hgap; j++) {
+                
+        //     }
+
+        // }
 
         
-        System.out.println(dap);
+        // System.out.println(dap);
 
-        sc.close();
+        // sc.close();
     }
 }
