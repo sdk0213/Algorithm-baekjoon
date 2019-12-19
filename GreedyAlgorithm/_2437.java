d599f01361ce3e49185039aaf922beccc92d727b
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-12-19 18:50:37 +0900

    191219

--- GreedyAlgorithm/_2437.java
+++ GreedyAlgorithm/_2437.java
@@ -1,11 +1,34 @@
 package GreedyAlgorithm;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class _2437 {
     static int weight[];
-    static int d[];
-    public static void main(String args[]){
+    static int pivot, sum;
+
+    static void ck_continuity() {
+
+        for (int i = 1; i < weight.length; i++) {
+            if (weight[i] - weight[i - 1] <= 1)
+                pivot = i;
+            else
+                break;
+        }
+    }
+
+    static void find_min() {
+        for (int i = pivot+1; i < weight.length; i++) {
+            if (weight[i] > (sum + 1)){
+                break;
+            }
+            else
+                sum += weight[i];
+        }
+
+    }
+
+    public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
 
@@ -14,24 +37,38 @@ public class _2437 {
         for (int i = 0; i < N; i++) {
             weight[i] = sc.nextInt();
         }
+        Arrays.sort(weight);
 
-        d = new int[1 << N];
-        int count=1;
-        d[0] = 0;
-        for (int i = 0; i < weight.length ; i++) {
-            for (int j = 0; j < 1 << i ; j++) {
-                d[count] = d[j] + weight[i];
-                count++;
-            }
+
+        if (Arrays.binarySearch(weight, 1) < 0) // 이진탐색으로 1이 없다면 1출력
+            System.out.println("1");
+        else if(N==1){
+            if(weight[0] != 1)
+                System.out.println("1");
+            else   
+                System.out.println("2");
         }
+        else{
 
-        for (int i = 0; i < d.length; i++) {
+            ck_continuity(); // 연속성검사
             
+            for (int i = 0; i <= pivot; i++) {
+                sum += weight[i];
+            }
+            // for (int i = 0; i < weight.length; i++) {
+            //     System.out.print(weight[i]+" ");
+            // }
+            // System.out.println();
+            // System.out.println("pivot : " + pivot + " sum : "+ sum );
+            if(pivot == weight.length-1)
+                System.out.println(sum+1);
+            else{
+                find_min();
+                System.out.println(sum+1);
+            }
+
         }
 
-
-
-        
         sc.close();
 
     }
