abf41c19db7d82fb5e633b1d874742db86c91ad1
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-12-28 15:46:18 +0900

    191228

--- GreedyAlgorithm/_1700.java
+++ GreedyAlgorithm/_1700.java
@@ -3,38 +3,22 @@ package GreedyAlgorithm;
 import java.util.Scanner;
 
 public class _1700 {
-    static int N;
+    static int N,K;
     static int multi[];
-    static int in[][];
-    static int repeat=0;
+    static int in[];
 
-    static boolean compare(int c,int t,int b){ 
-        if(c == N) 
-            return true;
-        if(in[b][c] != multi[t]){
-            // System.out.println("in["+b+"]["+c+"] != multi["+t+"]");
-            return compare(c+1,t,b);
-        }
+    static boolean compare(int i){ 
+        for (int j = 0; j < N; j++) 
+            if(in[i] == multi[j]) return true;
         return false;
     }
-    static void finder(int a,int b){ // 서로다른숫자를 in에다가 넣기
-        // System.out.println("a: " + a);
-        int count=0;
-        for (int i = a; i < multi.length; i++) {
-            if(compare(0,i,b) == true){ // in[b][0] != multi[i] && in[b][1] != multi[i]
-                in[b][count] = multi[i];
-                count++;
-            }
-            if (count == N){
-                // System.out.println("done");
-                // for (int j = 0; j < in.length; j++) {
-                //     System.out.print(in[j] + " ");
-                // }
-                // System.out.println();
-                finder(i+1,b+1);
-                break;
-            }
+
+    static boolean use(int a,int i){
+        for (int j = 1; j <= N; j++) {
+            if(multi[a] == in[i+j]) return true;
         }
+        
+        return false;
     }
 
 
@@ -42,57 +26,28 @@ public class _1700 {
 
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        int K = sc.nextInt();
-        multi = new int[K];
-        in = new int[(K/N)+1][N];
+        K = sc.nextInt();
+        multi = new int[N]; // 멀티탭
+        in = new int[K];
+        int result=0;
 
         for (int i = 0; i < K; i++) 
-            multi[i] = sc.nextInt();
+            in[i] = sc.nextInt();
 
-        finder(0,0);
-
-        // for (int i = 0; i < in.length; i++) {
-        //     for (int j = 0; j < N; j++) {
-        //         System.out.print(in[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-
-        int sum=0;
-        for (int i = 0; i < in.length-1; i++) {
-            if(in[i+1][0] == 0) break;
-            for (int j = 0; j < N; j++) {
-                for (int j2 = 0; j2 < N; j2++) {
-                    if(in[i][j] == in[i+1][j2]){
-                        repeat++;
+        for (int i = 0; i < K; i++) {
+            if(compare(i)==false){
+                for (int j = 0; j < N; j++) 
+                    if(use(j,i)==false){
+                        
+                        result++;    
                     }
-                }
             }
-            
-            int at=0;
-            for (int j2 = 0; j2 < N; j2++) {
-                if(in[i+1][j2] == 0){
-                    at++;
-                }        
-            }
-            // System.out.println("at : " + at);
-            
-    
-            repeat = N - repeat -at;
-            // System.out.println(repeat);
-            sum+=repeat;
-            repeat=0;
 
         }
-        System.out.println(sum);
 
+        
+        sc.close();
 
-        // if(repeat - N < 0)
-        //     System.out.println("0");
-        // else
-        //     System.out.println(repeat - N);
-
-        sc.close(); 
         
 
     }
--- interview/_marueng.java
+++ interview/_marueng.java
@@ -8,79 +8,73 @@ public class _marueng {
     
     static StringBuffer sb = new StringBuffer();
 
-    static void BinaryToOctal(int A){ // 2진수 -> Byte ( -128 ~ 127 바꾸기)
+    static byte BinaryToOctal(int A){ // 10진수 -> 음수표현가능한 Byte로 변환
         if(A > 127){
-            A--;
-            A = ~A;
-
-            System.out.println(String.format("%8s", Integer.toBinaryString(A & 0xFF)).replace(' ', '0'));
+            A = A - 128;
+            A = -128 + A;
+            return (byte)A;
         }
-        System.out.println(String.format("%8s", Integer.toBinaryString(A & 0xFF)).replace(' ', '0'));
-        System.out.println(A);
-        System.out.println(~A);
-        System.out.println(~A + 1);
-        System.out.println(String.format("%8s", Integer.toBinaryString((~A+1) & 0xFF)).replace(' ', '0'));
+        else{
+            return (byte)A;
+        }
     }
 
     static void MergeArray_bitByte(byte inArr1[], byte inArr2[] // Arr input
             , int ByteCnt_1, int BitCnt_1, int ByteCnt_2, int BitCnt_2, // input // TotalbyteCnt = 총합 , outArr = 출력
             int TotalbyteCnt, byte outArr[]) throws NumberFormatException  
     { 
-        System.out.print("랜덤입력 : Arr1 ByteCnt_1 : " + ByteCnt_1 + ", BitCnt_1 : " + BitCnt_1+ " ,");
-        System.out.println("Arr2 ByteCnt_2 : " + ByteCnt_2 + ", BitCnt_2 : " + BitCnt_2);
         for (int i = 0; i < ByteCnt_1; i++)
-            sb.append(String.format("%8s", Integer.toBinaryString(inArr1[i] & 0xFF)).replace(' ', '0'));
+            sb.append(String.format("%8s", Integer.toBinaryString(inArr1[i] & 0xFF)).replace(' ', '0')); // inArr1 앞부분 byte단위로 붙히기
 
-        String A = String.format("%8s", Integer.toBinaryString(inArr1[ByteCnt_1] & 0xFF)).replace(' ', '0');
+        String A = String.format("%8s", Integer.toBinaryString(inArr1[ByteCnt_1] & 0xFF)).replace(' ', '0'); //bit단위로 나누기
         String arr1[] = A.split("");
 
-        String B = String.format("%8s", Integer.toBinaryString(inArr2[ByteCnt_2] & 0xFF)).replace(' ', '0');
+        String B = String.format("%8s", Integer.toBinaryString(inArr2[ByteCnt_2] & 0xFF)).replace(' ', '0'); //bit단위로 나누기
         String arr2[] = B.split("");
 
         if (BitCnt_1 > 0)
-            for (int i = 0; i <= BitCnt_1; i++)
+            for (int i = 0; i <= BitCnt_1; i++) 
                 sb.append(arr1[i]);
         if (BitCnt_2 > 0)
-            for (int i = BitCnt_2; i < arr2.length; i++)
+            for (int i = BitCnt_2; i < arr2.length; i++) // bit로 나누어서 합친것이 byte를 넘어갈경우 byte하나 더 만들기
                 sb.append(arr2[i]);
 
-        for (int i = (ByteCnt_2 + 1); i < inArr2.length; i++)
+        for (int i = (ByteCnt_2 + 1); i < inArr2.length; i++) // inArr2 뒷부분 byte단위로 붙히기
             sb.append(String.format("%8s", Integer.toBinaryString(inArr2[i] & 0xFF)).replace(' ', '0'));
 
-        TotalbyteCnt = sb.length();
+        TotalbyteCnt = sb.length(); // 0을붙혀서 8자리 채우기
+
         if(TotalbyteCnt % 8 != 0)
         for (int i = 0; i < (8-(TotalbyteCnt % 8)); i++) 
             sb.append("0");
-        TotalbyteCnt = sb.length();
+        TotalbyteCnt =  sb.length()/8; // 총 byte 입력
+        System.out.println("TotalByteCnt : " + TotalbyteCnt);
 
-        System.out.println("출력어레이 : (TotalbyteCnt : "+TotalbyteCnt+" )");
-
-        for (int i = 0; i < (sb.length() / 8) + 1; i++)
-            if ((i * 8) + 8 <= sb.length())
-                System.out.print(sb.substring(i * 8, (i * 8) + 8) + " ");
-
-        System.out.println();
-
-
-        for (int i = 0; i < (sb.length() / 8); i++) 
+        for (int i = 0; i < (sb.length() / 8); i++)  
             if ((i * 8) + 8 <= sb.length()) {
-                
-                System.out.print(Byte.parseByte(sb.substring(i * 8, (i * 8) + 8), 2)+" ");
-                // outArr[i] = Integer.parseInt(sb.substring(i * 8, (i * 8) + 8), 2)+"");
+                outArr[i] = BinaryToOctal(Integer.parseInt(sb.substring(i * 8, (i * 8) + 8), 2)); // 8자리씩 끊어서 10진수로 변환후 BinrayToOctal함수
             }
+        System.out.print("outArr[] :\n");  
+        for (int i = 0; i < outArr.length; i++) 
+            System.out.print(outArr[i]+" ");
+        System.out.println();
+        for (int i = 0; i < outArr.length; i++) {
+            System.out.print(String.format("%8s", Integer.toBinaryString(outArr[i] & 0xFF)).replace(' ', '0')+" ");
+        }
         
 
     }
 
     public static void main(String args[]) throws IOException, NumberFormatException {
 
+
         Random rd = new Random();
 
-        System.out.println("입력 배열 랜덤입력 (8byte ~ 15byte)");
+        System.out.println("\n입력 배열 랜덤입력 (4byte ~ 8byte)");
 
         int size[] = new int[2];
-        size[0] = rd.nextInt(8) + 8;
-        size[1] = rd.nextInt(8) + 8;
+        size[0] = rd.nextInt(5) + 4;
+        size[1] = rd.nextInt(5) + 4;
 
         byte inArr[][] = new byte[2][];
 
@@ -88,11 +82,12 @@ public class _marueng {
         inArr[1] = new byte[size[1]];
 
         for (int i = 0; i < 2; i++) {
-            System.out.print("입력 배열 " + (i + 1) + " (" + size[i] + "byte)" + " :\n");
+            System.out.print("\n입력 배열 " + (i + 1) + " (" + size[i] + "byte)" + " :\n");
             for (int j = 0; j < size[i]; j++) {
                 inArr[i][j] = Byte.parseByte((rd.nextInt(256) - 128) + "");
-                // System.out.print(inArr[i][j] + " ");
+                System.out.print(inArr[i][j] + " ");
             }
+            System.out.println();
             for (int j2 = 0; j2 < size[i]; j2++) {
                 System.out.print(String.format("%8s", Integer.toBinaryString(inArr[i][j2] & 0xFF)).replace(' ', '0') + " ");
                 // 1,2의보수 계산하여 2진수 변환하는것이 시간이 오래걸릴것같아서
@@ -102,10 +97,18 @@ public class _marueng {
             System.out.println();
         }
 
-        byte outArr[] = new byte[size[0] + size[1]];
-
-        MergeArray_bitByte(inArr[0], inArr[1], rd.nextInt(inArr[0].length) + 0, rd.nextInt(8) + 0,
-                rd.nextInt(inArr[1].length) + 0, rd.nextInt(8) + 0, 0, outArr);
+        int ByteCnt_1 = rd.nextInt(inArr[0].length) + 0; // 변수 랜덤 입력
+        int BitCnt_1 = rd.nextInt(8) + 0;
+        int ByteCnt_2 = rd.nextInt(inArr[1].length) + 0;
+        int BitCnt_2 = rd.nextInt(8) + 0;
+        int make = (ByteCnt_1) + (size[1] - ByteCnt_2); // outArr 배열 필요한만큼 생성
+        if ( ( BitCnt_1 + 1) + (7 - BitCnt_2 + 1) > 8) make++;
+        byte outArr[] = new byte[make];
+        System.out.println();
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("Random Input : " + ByteCnt_1 + " " + BitCnt_1+ " " + ByteCnt_2 + " " + BitCnt_2+" (ByteCnt1,BitCnt1,ByteCnt2,BitCnt2)");
+        System.out.println("---------------------------------------------------------------------------------");
+        MergeArray_bitByte(inArr[0], inArr[1], ByteCnt_1, BitCnt_1 ,ByteCnt_2, BitCnt_2 , 0, outArr);
 
     }
 }
