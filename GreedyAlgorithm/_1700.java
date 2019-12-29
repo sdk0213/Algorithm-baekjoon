a1ac09819020189ac01927025d7a4806203ff8c0
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-12-29 17:50:04 +0900

    191229

--- GreedyAlgorithm/_1700.java
+++ GreedyAlgorithm/_1700.java
@@ -3,24 +3,29 @@ package GreedyAlgorithm;
 import java.util.Scanner;
 
 public class _1700 {
-    static int N,K;
+    static int N, K;
     static int multi[];
     static int in[];
 
-    static boolean compare(int i){ 
-        for (int j = 0; j < N; j++) 
-            if(in[i] == multi[j]) return true;
+    static boolean compare(int i) {
+        for (int j = 0; j < N; j++)
+            if (in[i] == multi[j])
+                return true;
         return false;
     }
 
-    static boolean use(int a,int i){
-        for (int j = 1; j <= N; j++) {
-            if(multi[a] == in[i+j]) return true;
+    static boolean use(int a, int i) {
+        for (int j = 1; j <= 2 * (N - 1); j++) {
+            if ((i + j) >= in.length)
+                continue;
+            if (multi[a] == in[i + j]) {
+                // System.out.println("multi["+a+"] == in["+i+j+"]");
+                return true;
+            }
         }
-        
+
         return false;
     }
-
 
     public static void main(String args[]){
 
@@ -31,19 +36,53 @@ public class _1700 {
         in = new int[K];
         int result=0;
 
-        for (int i = 0; i < K; i++) 
+        int count=0;
+        int last = 0;
+
+        for (int i = 0; i < K; i++)
             in[i] = sc.nextInt();
+ 
 
         for (int i = 0; i < K; i++) {
-            if(compare(i)==false){
-                for (int j = 0; j < N; j++) 
-                    if(use(j,i)==false){
-                        
-                        result++;    
-                    }
+            if (compare(i) == false) {
+                multi[count] = in[i];
+                count++;
+                last = i;
+                if(count == N) break;
             }
+        }
+
+        for (int i = 0; i < multi.length; i++) {
+            System.out.print(multi[i]+"");
+        }
+        
+        
+        
+
+        for (int i = last; i < K; i++) {
+            if(compare(i)==false){
+                for(int j = 0; j < N; j++) {
+                    if(j==N-1){
+                        result++;
+                        multi[j] = in[i];
+                        System.out.println("out");
+                        System.out.println("result : "+result + " multi : " +multi[0]+""+multi[1]+""+multi[2]); 
+                        break;
+                    } 
+                    if(use(j,i)==false){ // j 멀티탭 i 들어올거
+                        result++;
+                        multi[j] = in[i];
+                        System.out.println("result : "+result + " multi : " +multi[0]+""+multi[1]+""+multi[2]); 
+                        break;
+                    } 
+                }
+                 
+            }
+             
 
         }
+
+        System.out.println(result);
 
         
         sc.close();
