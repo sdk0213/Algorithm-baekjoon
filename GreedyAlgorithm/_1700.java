035bd673f59eb71fea134a61c6e30436c77501f6
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2019-12-30 19:58:01 +0900

    191230

--- GreedyAlgorithm/_1700.java
+++ GreedyAlgorithm/_1700.java
@@ -6,88 +6,87 @@ public class _1700 {
     static int N, K;
     static int multi[];
     static int in[];
+    static boolean m[];
+    static int result = 0;
 
-    static boolean compare(int i) {
+    static boolean compare(int i) { // 멀티탭안에 들어갈수가 이미 있는가
         for (int j = 0; j < N; j++)
             if (in[i] == multi[j])
                 return true;
         return false;
     }
 
-    static boolean use(int a, int i) {
-        for (int j = 1; j <= 2 * (N - 1); j++) {
-            if ((i + j) >= in.length)
-                continue;
-            if (multi[a] == in[i + j]) {
-                // System.out.println("multi["+a+"] == in["+i+j+"]");
-                return true;
+    static int empty(int j) {
+        for (int i = 0; i < N; i++) {
+            if (m[i] == false) { // 비어있다면
+                return i;
             }
         }
-
-        return false;
+        return -1; // 비어있는곳이 없다면
     }
 
-    public static void main(String args[]){
+    static int future(int i) {
+        int count = 0;
+        int max = 0;
+        int multi_max = 0;
+        for (int j = 0; j < N; j++) {
+            for (int j2 = i + 1; j2 < K; j2++) {
+                if (multi[j] == in[j2])
+                    break;
+
+                count++;
+            }
+            if (max < count) {
+                max = count;
+                multi_max = j;
+            }
+            // System.out.println("multi["+j+"] = "+multi_max + " count = "+ count);
+
+            count = 0;
+        }
+        return multi_max;
+
+    }
+
+    public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         K = sc.nextInt();
         multi = new int[N]; // 멀티탭
+        m = new boolean[N];
         in = new int[K];
-        int result=0;
-
-        int count=0;
-        int last = 0;
 
         for (int i = 0; i < K; i++)
             in[i] = sc.nextInt();
- 
+        for (int i = 0; i < N; i++)
+            m[i] = false;
+
+        // 같은수가 있는가
+        // 비어있는가
+        // 둘다 아니라면 후에 가장 뒤에 나오는것을 먼저 뽑기
 
         for (int i = 0; i < K; i++) {
-            if (compare(i) == false) {
-                multi[count] = in[i];
-                count++;
-                last = i;
-                if(count == N) break;
-            }
-        }
+            if (compare(i) == true) { // 같은수가 있다면
+                continue;
+            } else { // 같은수가 없다면
+                int e;
+                if ((e = empty(i)) != -1) { // 빈공간있다면
+                    multi[e] = in[i];
+                    m[e] = true;
+                } else { // 빈공간없다면
+                    multi[future(i)] = in[i]; // 가장 뒤에 나오는것을 먼저 뽑기
+                    result++;
 
-        for (int i = 0; i < multi.length; i++) {
-            System.out.print(multi[i]+"");
-        }
-        
-        
-        
-
-        for (int i = last; i < K; i++) {
-            if(compare(i)==false){
-                for(int j = 0; j < N; j++) {
-                    if(j==N-1){
-                        result++;
-                        multi[j] = in[i];
-                        System.out.println("out");
-                        System.out.println("result : "+result + " multi : " +multi[0]+""+multi[1]+""+multi[2]); 
-                        break;
-                    } 
-                    if(use(j,i)==false){ // j 멀티탭 i 들어올거
-                        result++;
-                        multi[j] = in[i];
-                        System.out.println("result : "+result + " multi : " +multi[0]+""+multi[1]+""+multi[2]); 
-                        break;
-                    } 
                 }
-                 
             }
-             
-
+            // for (int j = 0; j < multi.length; j++) {
+            // System.out.print(multi[j] + " ");
+            // }
+            // System.out.println();
         }
 
         System.out.println(result);
-
-        
-        sc.close();
-
-        
 
     }
 }
