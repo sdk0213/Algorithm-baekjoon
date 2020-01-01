bc0fe438a2cfd7803f7f68f6301f14516dc47461
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2020-01-01 17:49:03 +0900

    200101

--- GreedyAlgorithm/_1507.java
+++ GreedyAlgorithm/_1507.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class _1507 {
 
+    // X -> Y 로 갈때
+    // {Z1,Z2,Z3} 의 경우의수를 전부 고려하였을때 비용이 미만이라면 그 길을 채택해야됨
+    // 1) 그러므로 Z1,Z2,Z3 을 전부 나열하는 방법의 수를 고려해야함
+    // 모든경우의수는 어떤식으로 구할수 있을까?
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
