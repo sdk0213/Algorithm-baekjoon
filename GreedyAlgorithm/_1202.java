d44efe67930afce1ff474ea6095bb1a891983dad
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2020-01-23 17:33:31 +0900

    200123

--- GreedyAlgorithm/_1202.java
+++ GreedyAlgorithm/_1202.java
@@ -1,48 +1,12 @@
 package GreedyAlgorithm;
 
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
+import com.oracle.webservices.internal.api.EnvelopeStyle;
+
 /*
-1. 주어진 보석을 무게를 기준으로 정렬한다. 그리고 가격을 기준으로 정렬한다.
-2. 입력받은 가방을 오름차순으로 정렬한다.
-3. 무게가 가장낮은것부터 계산하여서 가방에 들어가는 보석중 가장 가치있는것부터 가방에 집어넣는다.
-4. 가방 무게가 낮은것부터 하는 이유는 높은것부터 할경우 가장 작은가방에 들어가도 될 보석을 큰가방에 집어넣음으로써 실패할수있기 때문이다.
-*/
-public class _1202{
-
-    public static void main(String args[]){
-
-        Scanner sc = new Scanner(System.in);
-        int jewelry_Num = sc.nextInt();
-        int bag_Num = sc.nextInt();
-        int jewelry[][] = new int[jewelry_Num][2];
-        int bag[] = new int[bag_Num];
-
-        for (int i = 0; i < jewelry_Num; i++) {
-            jewelry[i][0] = sc.nextInt();
-            jewelry[i][1] = sc.nextInt();
-        }
-
-        for (int i = 0; i < bag_Num; i++) 
-            bag[i] = sc.nextInt();
-
-
-        Arrays.sort(jewelry, (o1,o2) -> {
-            if(o1[0] == o2[0])
-                return Integer.compare(o1[1], o2[1]);
-            else
-                return Integer.compare(o1[0],o2[0]);
-
-        });
-        
-
-        for (int i = 0; i < jewelry.length; i++) {
-            System.out.println(jewelry[i][0] + " / " + jewelry[i][1]);
-            
-        }
-        
         // jewelry[][] 를 오름차순으로 정렬(1.무게 2.가격)
         // bag[i] 를 오름차순으로 정렬
         // while(){
@@ -50,12 +14,59 @@ public class _1202{
         //  }
         // 검색할때는 bag[i] 보다 작은것들을 전부 검색해야하고 그중에서 가장 값어치 있는것을 넣어야함
         // 5의 공간의 가방에 들어갈 쥬얼리를 찾을때는 1,2,3,4,5 중(정렬되어있음) 에 가장 가치가 높은것을 찾으면됨
-        //  
+*/
+
+
+
+class Jewel_Info implements Comparable<Jewel_Info>{
+    int Jewel_Info_Weight;
+    int Jewel_Info_Price;
+
+    public Jewel_Info(int Jewel_Info_Weight,int Jewel_Info_Price){
+        this.Jewel_Info_Weight = Jewel_Info_Weight;
+        this.Jewel_Info_Price = Jewel_Info_Price;
+    }
+    public int compareTo(Jewel_Info ObjforCompare) {
+        if(this.Jewel_Info_Weight == ObjforCompare.Jewel_Info_Weight)
+            if(this.Jewel_Info_Price < ObjforCompare.Jewel_Info_Price)
+                return 1;
+            else    
+                return -1;
+        else
+            if(this.Jewel_Info_Weight > ObjforCompare.Jewel_Info_Weight)
+                return 1;
+            else
+                return -1;
+    }
+}
+public class _1202{
+
+    public static void main(String args[]){
+
+
+        Scanner inputScanner = new Scanner(System.in);
+        int Jeweler_had_Jewel = inputScanner.nextInt();
+        int SangDuk_had_Bag = inputScanner.nextInt();
+        int SangDuk_had_Bag_Info[] = new int[SangDuk_had_Bag];
+        PriorityQueue<Jewel_Info> priorityQueue = new PriorityQueue<Jewel_Info>();
+
+        for (int i = 0; i < Jeweler_had_Jewel; i++)
+            priorityQueue.offer(new Jewel_Info(inputScanner.nextInt(), inputScanner.nextInt()));
+        for (int i = 0; i < SangDuk_had_Bag; i++)
+            SangDuk_had_Bag_Info[i] = inputScanner.nextInt();
+
+        Arrays.sort(SangDuk_had_Bag_Info);
         
+         while(!priorityQueue.isEmpty()){
+            Jewel_Info jewel_Info = priorityQueue.poll();
+            System.out.println(jewel_Info.Jewel_Info_Weight +" "+ jewel_Info.Jewel_Info_Price);
+        }
 
+        for (int i = 0; i < SangDuk_had_Bag; i++) {
+                        
+        }
 
-    
-        sc.close();
+        inputScanner.close();
 
 
         
