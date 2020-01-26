a91837dbfd288c24c23c5c3c27d03f70489d3bf0
Sung DaeKyoung <51182964+sdk0213@users.noreply.github.com>
2020-01-26 19:14:40 +0900

    200126

--- GreedyAlgorithm/_1202.java
+++ GreedyAlgorithm/_1202.java
@@ -26,16 +26,7 @@ class Jewel_Info implements Comparable<Jewel_Info>{
         this.Jewel_Info_Price = Jewel_Info_Price;
     }
     public int compareTo(Jewel_Info ObjforCompare) {
-        if(this.Jewel_Info_Weight == ObjforCompare.Jewel_Info_Weight)
-            if(this.Jewel_Info_Price < ObjforCompare.Jewel_Info_Price)
-                return 1;
-            else    
-                return -1;
-        else
-            if(this.Jewel_Info_Weight > ObjforCompare.Jewel_Info_Weight)
-                return 1;
-            else
-                return -1;
+        return ObjforCompare.Jewel_Info_Weight - this.Jewel_Info_Weight;
     }
 }
 public class _1202{
@@ -46,19 +37,28 @@ public class _1202{
         Scanner inputScanner = new Scanner(System.in);
         int Jeweler_had_Jewel = inputScanner.nextInt();
         int SangDuk_had_Bag = inputScanner.nextInt();
+        Jewel_Info jewel_Info[] = new Jewel_Info[Jeweler_had_Jewel];
         int SangDuk_had_Bag_Info[] = new int[SangDuk_had_Bag];
         PriorityQueue<Jewel_Info> priorityQueue = new PriorityQueue<Jewel_Info>();
 
+
         for (int i = 0; i < Jeweler_had_Jewel; i++)
-            priorityQueue.offer(new Jewel_Info(inputScanner.nextInt(), inputScanner.nextInt()));
+            jewel_Info[i] = new Jewel_Info(inputScanner.nextInt(), inputScanner.nextInt());
         for (int i = 0; i < SangDuk_had_Bag; i++)
             SangDuk_had_Bag_Info[i] = inputScanner.nextInt();
-
+        Arrays.sort(jewel_Info);
         Arrays.sort(SangDuk_had_Bag_Info);
         
-         while(!priorityQueue.isEmpty()){
-            Jewel_Info jewel_Info = priorityQueue.poll();
-            System.out.println(jewel_Info.Jewel_Info_Weight +" "+ jewel_Info.Jewel_Info_Price);
+        // for (int Putin = 0; Putin < Jeweler_had_Jewel; Putin++) {
+            
+        //     while( SangDuk_had_Bag > 0 && jewel_Info.Jewel_Info_Weight < SangDuk_had_Bag_Info[j] ){
+
+        //     }
+            
+        // }
+    
+        for (int i = 0; i < Jeweler_had_Jewel; i++) {
+            System.out.println(jewel_Info[i].Jewel_Info_Weight + " " + jewel_Info[i].Jewel_Info_Price);
         }
 
         for (int i = 0; i < SangDuk_had_Bag; i++) {
@@ -67,9 +67,6 @@ public class _1202{
 
         inputScanner.close();
 
-
-        
-        
 
     }
 }
